6ebec10a5ec0395022748e8fdac08a6db814fe1a
andrewdemaris <47347848+andrewdemaris@users.noreply.github.com>
2022-05-26 22:05:41 -0700

    updated store n stuff

---
+++ customerdatabase.h
@@ -0,0 +1,22 @@
+#ifndef CUSTOMERDATABASE_H
+#define CUSTOMERDATABASE_H
+#pragma once;
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class CustomerDatabase
+{
+    public:
+        CustomerDatabase();
+        ~CustomerDatabase();
+
+
+    private:
+
+
+};
+
+
+#endif // CUSTOMERDATABASE_H
--- store.cpp
+++ store.cpp
@@ -1,16 +1,95 @@
 #include "store.h"
+#include "customerdatabase.h"
 
-void Store::readCommands(ifstream &fileName)
+Store::Store()
 {
+    Inventory StoreInventory = new Inventory;
+    CustomerDatabase StoreCustomerDatabase = new CustomerDatabase;
 
 }
 
-void Store::readCustomers(ifstream &fileName)
+void Store::readCommands(string &fileName)
 {
+    string currentCommand = "";
+    ifstream data;
+    data.open(fileName);
+    if (!data)
+    {
+        cerr << "Error: file '" << fileName << "' could not be opened" << endl;
+        exit(1);
+    }
+    data >> currentCommand;
+    while ( !data.eof() )
+    {
+        switch( (int)currentCommand[0] )
+        {
+            case 66: // B
+            
+                break;
+            case 72: // H
 
+                break;
+            case 73: // I
+
+                break;
+            case 82: // R
+
+                break;
+            default:
+                cerr << "Error: invalid command type '" << currentCommand[0] << "'" << endl;
+        }
+    }
 }
 
-void Store::readMovies(ifstream &fileName)
+void Store::readCustomers(string &fileName)
 {
+    int customerID;
+    string custFirst;
+    string custLast;
+    ifstream data;
+    string forHash;
+    data.open(fileName);
+    if (!data)
+    {
+        cerr << "Error: file '" << fileName << "' could not be opened" << endl;
+        exit(1);
+    }
+    
+    while ( !data.eof() )
+    {
+        data >> customerID >> custFirst >> custLast;
+        forHash = (custFirst.append(custLast.append(to_string(customerID))));
+        // add a customer with hash of the string of all the above
+        // and Customer(customerID, custFirst, custLast);
+    }
+}
 
+void Store::readMovies(string &fileName)
+{
+    string currentCommand = "";
+    ifstream data;
+    data.open(fileName);
+    if (!data)
+    {
+        cerr << "Error: file '" << fileName << "' could not be opened" << endl;
+        exit(1);
+    }
+    data >> currentCommand;
+    while ( !data.eof() )
+    {
+        switch( (int)currentCommand[0] )
+        {
+            case 67: // C
+            
+                break;
+            case 68: // D
+
+                break;
+            case 70: // F
+                
+                break;
+            default:
+                cerr << "Error: invalid movie type '" << currentCommand[0] << "'" << endl;
+        }
+    }
 }
