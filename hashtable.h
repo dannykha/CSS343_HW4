6485c0611dbff0b414d4f4511ef134c0b2967b42
Danny <danny.n.kha@gmail.com>
2022-05-26 23:59:14 -0700

    hashtable implenmentation

--- hashtable.h
+++ hashtable.h
@@ -1,16 +1,34 @@
 #ifndef HASTABLE_H
 #define HASTABLE_H
 #pragma once;
+#define TABLE_SIZE 250
+#include "customer.h"
 
 using namespace std;
+
+class HashElement
+{
+    public:
+        int key;
+        Customer *customer;
+        HashElement(int k, Customer *cust) {
+            key = k;
+            customer = cust;
+        }
+};
 
 class HashTable
 {
     public:
-
+        HashTable();
+        ~HashTable();
+        void destroy();
+        int hashFunction(int key);
+        void insert(int key, Customer *custPtr);
+        Customer *search(int key);
+        void remove(int key);
     private:
-    
-    
+        HashElement **table;
 };
 
 
