274e1582d22f35efc4147bf3469bb981930c69f3
ssaleh01 <ssaleh@uw.edu>
2022-06-07 12:26:45 -0700

    customer borrow and return edge cases

--- customer.cpp
+++ customer.cpp
@@ -71,21 +71,36 @@ void Customer::addMovie(Movie &movie)
 //borrow function; stock - 1
 void Customer::borrowMovie(Movie &movie)
 {
-    addMovie(movie);
     string temp = movie.getGenre() + movie.getDirector() + 
         to_string(movie.getReleaseYear()) + movie.getTitle();
 	int key = stoi(temp);
-    Store::StoreInventory.search(key)->
-        setStock(Store::StoreInventory.search(key)->getStock() - 1);
-    string message = "User borrowed: " + movie.getReleaseYear()
-         + movie.getTitle(); 
-    transactionHistory.push_back(message);
+
+    //edge-case check if movie exists and in stock
+    if (Store::StoreInventory.search(key) == nullptr ||
+    Store::StoreInventory.search(key)->getStock() == 0)
+    {
+        string errorM = "Sorry! " + movie.getReleaseYear() + 
+            movie.getTitle() + " is unavailable.";
+        transactionHistory.push_back(errorM);
+    }
+    else
+    {
+        addMovie(movie);  
+        Store::StoreInventory.search(key)->
+            setStock(Store::StoreInventory.search(key)->getStock() - 1);
+        string message = "User borrowed: " + movie.getReleaseYear()
+            + movie.getTitle(); 
+        transactionHistory.push_back(message);
+    };
 };
 
 //-----------------------------------------------------------------
 //return function; stock + 1
 void Customer::returnMovie(Movie &movie)
 {
+    //edge-case check if customer has the movie checked out
+    bool movieBorrowed = false;
+    
     for (int i = 0; i <= currentMovies.size(); i++)
     {
         if (currentMovies.at(i) == movie)
@@ -93,17 +108,28 @@ void Customer::returnMovie(Movie &movie)
             vector<Movie>::iterator it;
             it = currentMovies.begin() + i; // might need to be i - 1
             currentMovies.erase(it);
+            movieBorrowed = true;
         };
-    }
-    string temp = movie.getGenre() + movie.getDirector() + 
-        to_string(movie.getReleaseYear()) + movie.getTitle();
-	int key = stoi(temp);
-    Store::StoreInventory.search(key)->
-        setStock(Store::StoreInventory.search(key)->getStock() + 1);
+    };
 
-    string message = "User returned: " + movie.getReleaseYear() 
-        + movie.getTitle(); 
-    transactionHistory.push_back(message);    
+    //if not in customer inventory, print error
+    if (movieBorrowed == false)
+    {
+        string errorM = "Sorry! " + movie.getReleaseYear() + 
+            movie.getTitle() + " is not in User's inventory.";
+        transactionHistory.push_back(errorM);
+    }
+    else //perform return otherwise
+    {
+        string temp = movie.getGenre() + movie.getDirector() + 
+            to_string(movie.getReleaseYear()) + movie.getTitle();
+	    int key = stoi(temp);
+        Store::StoreInventory.search(key)->
+            setStock(Store::StoreInventory.search(key)->getStock() + 1);
+        string message = "User returned: " + movie.getReleaseYear() 
+            + movie.getTitle(); 
+        transactionHistory.push_back(message);    
+    };
 };
 
 //-----------------------------------------------------------------
