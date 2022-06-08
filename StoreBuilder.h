ab0e29f0f6ef74c21446a73b6346aa2c9d08d821
ssaleh01 <ssaleh@uw.edu>
2022-06-07 19:44:15 -0700

    store builder comments

--- StoreBuilder.h
+++ StoreBuilder.h
@@ -1,3 +1,14 @@
+// ----------------------------------------------------------------
+// StoreBuilder.h
+// Andrew Demaris, Danny Kha, Sara Saleh CSS343B 
+// Creation Date: May 18, 2022
+// Date of Last Modification: June 7, 2022
+// ----------------------------------------------------------------
+// Purpose - provide declarations for StoreBuilder.cpp file
+// ----------------------------------------------------------------
+// Notes on specifications, special algorithms, and assumptions:
+// ----------------------------------------------------------------
+
 #ifndef STOREBUILDER_H
 #define STOREBUILDER_H
 #pragma once
@@ -9,9 +20,16 @@ using namespace std;
 class StoreBuilder 
 {
     public:
+        //constructor
         StoreBuilder();
+
+        //destructor
         ~StoreBuilder();
+
+        //initialize store
         void addStore(string &);
+
+        //getter function
         Store getStore(int);
 
     private:
